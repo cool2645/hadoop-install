6a5987e7bc2e358428cc71b55c6ff2bf65f393ba
Harry <zhouziqun@cool2645.com>
2017-03-05 18:56:13 +0800

    Replace hadoop_password.

--- master.sh
+++ master.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #
+source hadoop_install.conf
 # Create user hadoop
 useradd -m hadoop -s /bin/bash
 passwd hadoop << EOF
-Cool2645!
-Cool2645!
+${hadoop_password}
+${hadoop_password}
 EOF
 adduser hadoop sudo
 su - hadoop << EOF
@@ -12,17 +13,17 @@ cd ~
 EOF
 
 # Install openjdk
-echo 'Cool2645!' | sudo -S apt-get update -y
-echo 'Cool2645!' | sudo -S apt-get dist-upgrade -y 
-echo 'Cool2645!' | sudo -S apt-get upgrade -y
-echo 'Cool2645!' | sudo -S apt-get install openjdk-7-jre openjdk-7-jdk
+echo ${hadoop_password} | sudo -S apt-get update -y
+echo ${hadoop_password} | sudo -S apt-get dist-upgrade -y 
+echo ${hadoop_password} | sudo -S apt-get upgrade -y
+echo ${hadoop_password} | sudo -S apt-get install openjdk-7-jre openjdk-7-jdk
 str=$(dpkg -L openjdk-7-jdk | grep '/bin/javac')
 JH=${str%%/bin/javac}
 echo 'export JAVA_HOME='$JH >> ~/.bashrc
 source ~/.bashrc
 
 # Config ssh
-echo 'Cool2645!' | sudo -S apt-get install openssh-server -y
+echo ${hadoop_password} | sudo -S apt-get install openssh-server -y
 mkdir ~/.ssh
 cd ~/.ssh/
 ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
@@ -32,7 +33,7 @@ cat ./id_rsa.pub >> ./authorized_keys
 cd ~
 wget http://mirror.bit.edu.cn/apache/hadoop/common/stable/hadoop-2.7.3.tar.gz
 mv hadoop*.tar.gz hadoop.tar.gz
-echo 'Cool2645!' | sudo -S tar -zxf ~/hadoop.tar.gz -C /usr/local
+echo ${hadoop_password} | sudo -S tar -zxf ~/hadoop.tar.gz -C /usr/local
 cd /usr/local/
-echo 'Cool2645!' | sudo -S mv ./hadoop-*/ ./hadoop
-echo 'Cool2645!' | sudo -S chown -R hadoop ./hadoop
+echo ${hadoop_password} | sudo -S mv ./hadoop-*/ ./hadoop
+echo ${hadoop_password} | sudo -S chown -R hadoop ./hadoop
